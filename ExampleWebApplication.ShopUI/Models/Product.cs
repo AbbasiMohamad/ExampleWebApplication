8053fa77826e0724c8a8b56df1ee7f6927bebe14
Mohamad <mohamad@myself>
2022-05-27 11:35:25 +0430

    add sample model (product)

---
+++ ExampleWebApplication.ShopUI/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleWebApplication.ShopUI.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+}
