e91f4a85e0c5e2e8e95aadb5241c8913782f7172
Mohamad <mohamad@myself>
2022-05-27 16:15:17 +0430

    add tag helper for pager feature

---
+++ ExampleWebApplication.ShopUI/Infrastructures/PagerTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using ExampleWebApplication.ShopUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ExampleWebApplication.ShopUI.Infrastructures;
+
+//اینجا میگوییم هدف ما چه المانی در صفحه با چه خصیصه ای است
+[HtmlTargetElement("div", Attributes = "paged-model")] 
+public class PagerTagHelper: TagHelper
+{
+    private readonly IUrlHelperFactory _urlHelperFactory;
+
+    public PagerTagHelper(IUrlHelperFactory urlHelperFactory)
+    {
+        _urlHelperFactory = urlHelperFactory;
+    }
+
+    [ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+    public PageInfo PagedModel { get; set; }
+    public string PageAction { get; set; }
+
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+        TagBuilder finalTag = new TagBuilder("div");
+        for (int i = 1; i <= PagedModel.PageCount; i++)
+        {
+            TagBuilder linkTag = new("a");
+            linkTag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNumber = i });
+            
+            linkTag.InnerHtml.Append(i.ToString());
+            finalTag.InnerHtml.AppendHtml(linkTag);
+        }
+        output.Content.AppendHtml(finalTag.InnerHtml);
+
+    }
+}
--- ExampleWebApplication.ShopUI/Models/PageInfo.cs
+++ ExampleWebApplication.ShopUI/Models/PageInfo.cs
@@ -5,5 +5,5 @@ public class PageInfo
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public int PageNumber { get; set; }
-    public int PageCount => (int)Math.Ceiling((double)(TotalCount / PageSize));
+    public int PageCount => (int)Math.Ceiling((double)TotalCount / PageSize);
 }
