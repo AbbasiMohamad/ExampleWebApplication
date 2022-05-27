7c70caee7b1280c76f1a40e966b8d6f3465c6986
Mohamad <mohamad@myself>
2022-05-27 11:04:27 +0430

    add basic configuration for mvc application in Program.cs

--- ExampleWebApplication.ShopUI/Program.cs
+++ ExampleWebApplication.ShopUI/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+/* این متد تمام آبجکت های مورد نیاز در خصوص کار
+ * با کنترلرها و ویوها را مهیا میکند*/
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    //این میدلور یک صفحه برای نمایش جزئیات اکسپشنها مهیا میکند
+    app.UseDeveloperExceptionPage();
+
+// ایجاد صفحه برای خطاهای مشخص بین خطاهای 400 تا 599
+app.UseStatusCodePages();
+
+// این میدلور امکان سِرو شدن فایل های استاتیک را مهیا میکند
+app.UseStaticFiles();
+
+// این میدلور امکان مسیریابی را مهیا میکند. درخواست کاربر را پردازش میکند و به مقصد نهایی میرساند
+app.UseRouting();
+
+
+app.UseEndpoints(endpoints =>
+    endpoints.MapDefaultControllerRoute() // {controller=home}/{action=index}/{id?}
+);
+
 
 app.Run();
