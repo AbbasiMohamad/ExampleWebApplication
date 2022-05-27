c5f309396d2767d1b63728488494d6d922078951
Mohamad <mohamad@myself>
2022-05-27 14:11:42 +0430

    add product list to index page

--- ExampleWebApplication.ShopUI/Controllers/HomeController.cs
+++ ExampleWebApplication.ShopUI/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExampleWebApplication.ShopUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleWebApplication.ShopUI.Controllers;
 public class HomeController : Controller
 {
+    private readonly IProductRepository _productRepository;
+
+    public HomeController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
     public IActionResult Index()
     {
-        return View();
+        return View(_productRepository.GetAll());
     }
 }
