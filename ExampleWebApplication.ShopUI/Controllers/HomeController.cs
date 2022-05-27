78b512a2c03fa8923edfb9652ee414f7b197197e
Mohamad <mohamad@myself>
2022-05-27 11:11:59 +0430

    add first controller

---
+++ ExampleWebApplication.ShopUI/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleWebApplication.ShopUI.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
