c269c7479ef75cbe2ecd3328547a27cbb9b40fc7
Mohamad <mohamad@myself>
2022-05-30 12:51:05 +0430

    add basket summary view component

---
+++ ExampleWebApplication.ShopUI/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace ExampleWebApplication.ShopUI.Components;
+
+public class BasketSummaryViewComponent : ViewComponent
+{
+    private readonly Basket _basketService;
+
+    public BasketSummaryViewComponent(Basket basketService)
+    {
+        _basketService = basketService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        
+        return View(_basketService);
+    }
+}
